7c11f3997e9ba6c802d6fc1cdfdd6db109d7d90d
gahyeonn <parka0017@gmail.com>
2022-03-25 18:21:48 +0900

    Add configuation
    Add dependency, resoure files(S3)

---
+++ src/main/java/com/gahyeonn/useofs3/UseOfS3Application.java
@@ -0,0 +1,13 @@
+package com.gahyeonn.useofs3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseOfS3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UseOfS3Application.class, args);
+    }
+
+}
